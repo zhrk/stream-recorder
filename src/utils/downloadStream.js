a34f32eeb60c62f706e0a0945d4be2a3de0e8004
zhrk <zshchipets@gmail.com>
2025-09-07 16:51:37 +0400

    fix streamlink command

--- src/utils/downloadStream.js
+++ src/utils/downloadStream.js
@@ -2,7 +2,6 @@ const { spawn } = require('child_process');
 
 /** @param {"twitch" | "kick"} platorm */
 const downloadStream = (platorm, username, channel_slug, startTime) => {
-  const exe = 'C:/Programs/streamlink/bin/streamlink.exe';
   const url = platorm === 'twitch' ? 'https://twitch.tv' : 'https://kick.com';
   const output = `C:/Users/PC/Desktop/server/vods/${username}-${startTime}.mp4`;
   const proxy = platorm === 'twitch' ? '--http-proxy "http://127.0.0.1:12334" ' : '';
@@ -14,7 +13,7 @@ const downloadStream = (platorm, username, channel_slug, startTime) => {
       '-1',
       'powershell',
       '-Command',
-      `${exe}`,
+      `streamlink`,
       `--hls-live-restart`,
       `--hls-playlist-reload-attempts 60`,
       `${proxy}${url}/${channel_slug}`,
