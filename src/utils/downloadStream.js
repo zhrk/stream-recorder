1687026d58a21e1f299ed354947735c5505b0465
zhrk <zshchipets@gmail.com>
2026-05-17 14:09:31 +0400

    fix download command

--- src/utils/downloadStream.js
+++ src/utils/downloadStream.js
@@ -1,27 +1,28 @@
 const { spawn } = require('child_process');
 const { logDownload } = require('../services/logger');
 
-const CREATE_NEW_CONSOLE = 0x00000010;
-
 const downloadStream = (...args) => {
   const [platorm, username, channel_slug, startTime, retry = 0] = args;
 
   const url = platorm === 'twitch' ? 'https://twitch.tv' : 'https://kick.com';
   const output = `C:/Users/PC/Desktop/server/vods/${username}-${startTime}.mp4`;
+  const proxy = platorm === 'twitch' ? '--http-proxy "http://127.0.0.1:12334" ' : '';
 
   const child = spawn(
-    'streamlink',
+    'wt',
     [
-      '--hls-live-restart',
-      '--hls-playlist-reload-attempts',
-      '60',
-      ...(platorm === 'twitch' ? ['--http-proxy', 'http://127.0.0.1:12334'] : []),
-      `${url}/${channel_slug}`,
-      'best',
-      '-o',
-      output,
+      '-w',
+      '-1',
+      'powershell',
+      '-Command',
+      `streamlink`,
+      `--hls-live-restart`,
+      `--hls-playlist-reload-attempts 60`,
+      `${proxy}${url}/${channel_slug}`,
+      `best`,
+      `-o ${output}`,
     ],
-    { stdio: 'ignore', windowsHide: false, creationFlags: CREATE_NEW_CONSOLE }
+    { stdio: 'ignore', detached: true }
   );
 
   child.on('exit', (code) => {
