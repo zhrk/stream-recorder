e8f508dd2866f745ed3743f6b67b920822bc67f8
zhrk <zshchipets@gmail.com>
2026-08-01 21:48:31 +0400

    disable tg client

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const twitchRoute = require('./routes/twitchRoute');
 const twitchConfigRoute = require('./routes/twitchConfigRoute');
 const kickRoute = require('./routes/kickRoute');
 const kickConfigRoute = require('./routes/kickConfigRoute');
-require('./services/telegramClient');
+// require('./services/telegramClient');
 
 const { port } = config;
 
