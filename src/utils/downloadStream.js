95f2eb1b3db1f9cabec417dd0d777cf38c9773bd
zhrk <zshchipets@gmail.com>
2026-09-02 22:45:30 +0400

    revert

--- src/utils/downloadStream.js
+++ src/utils/downloadStream.js
@@ -1,21 +1,5 @@
 const { spawn } = require('child_process');
 
-function findOxmgr() {
-  try {
-    return execFileSync('where', ['oxmgr'], {
-      encoding: 'utf8',
-      windowsHide: true,
-    })
-      .split(/\r?\n/)
-      .map((x) => x.trim())
-      .find(Boolean);
-  } catch {
-    return null;
-  }
-}
-
-const OXMGR = findOxmgr();
-
 const URLS = {
   twitch: 'https://twitch.tv',
   kick: 'https://kick.com',
@@ -26,30 +10,23 @@ const downloadStream = (...args) => {
 
   const url = URLS[platform];
   const output = `C:/Users/PC/Desktop/server/vods/${username}-${startTime}.mp4`;
-  const proxyArgs = platform === 'twitch' ? ['--http-proxy', 'http://127.0.0.1:12334'] : [];
-
-  const processName = `streamlink-${username}-${startTime}`;
-
-  const streamlinkCommand = [
-    'streamlink',
-    '--hls-live-restart',
-    '--hls-playlist-reload-attempts',
-    '60',
-    ...proxyArgs,
-    `${url}/${channel_slug}`,
-    'best',
-    '-o',
-    `"${output}"`,
-  ].join(' ');
+  const proxy = platform === 'twitch' ? '--http-proxy "http://127.0.0.1:12334" ' : '';
 
   spawn(
-    'cmd.exe',
-    ['/c', 'oxmgr', 'start', streamlinkCommand, '--name', processName, '--restart', 'never'],
-    {
-      stdio: 'ignore',
-      detached: true,
-      windowsHide: true,
-    }
+    'wt',
+    [
+      '-w',
+      '-1',
+      'powershell',
+      '-Command',
+      `streamlink`,
+      `--hls-live-restart`,
+      `--hls-playlist-reload-attempts 60`,
+      `${proxy}${url}/${channel_slug}`,
+      `best`,
+      `-o ${output}`,
+    ],
+    { stdio: 'ignore', detached: true }
   );
 };
 
